401b9d4cf163ddeca0338905085432f5919d1ad0
Sasha299 <sasha299@optonline.net>
2014-11-23 20:53:23 -0500

    class proj

--- run_analysis.R
+++ run_analysis.R
@@ -17,11 +17,11 @@ unlink(temp)
 ## Merges the training and the test sets to create one data set.
 ## read data from each table
 xtrainData <- read.table("projectData/UCI HAR Dataset/train/X_train.txt")
-ytrainData <- read.table("projectData/UCI HAR Dataset/train/y_train.txt")
-subjecttrainData <- read.table("projectData/UCI HAR Dataset/train/subject_train.txt")
+ytrainData <- read.table("projectData/UCI HAR Dataset/train/y_train.txt", col.names="activity")
+subjecttrainData <- read.table("projectData/UCI HAR Dataset/train/subject_train.txt", col.names="subject")
 xtestData <- read.table("projectData/UCI HAR Dataset/test/X_test.txt")
-ytestData <- read.table("projectData/UCI HAR Dataset/test/y_test.txt")
-subjecttestData <- read.table("projectData/UCI HAR Dataset/test/subject_test.txt")
+ytestData <- read.table("projectData/UCI HAR Dataset/test/y_test.txt", col.names="activity")
+subjecttestData <- read.table("projectData/UCI HAR Dataset/test/subject_test.txt", col.names="subject")
 ## Join x tain and test tables, join y train and test tables and join subject test and train tables
 xjoinData <- rbind(xtrainData, xtestData)
 yjoinData <- rbind(ytrainData, ytestData)
@@ -35,10 +35,12 @@ features <- read.table("projectData/UCI HAR Dataset/features.txt")
 ## from the features_info.txt file
 ## mean(): Mean value
 ## std(): Standard deviation
-meanData <- grep("mean()", features[, 2])
-stdData <- grep("std()", features[,2])
-xjoinData <- xjoinData[, meanData]
-xjoinData <- xjoinData[, stdData]
+## meanData <- grep("mean()", features[, 2])
+## stdData <- grep("std()", features[,2])
+meanstdData <- grep("mean|std", features[,2])
+## only the rows we want
+features2<- features[meanstdData,]
+## subjectjoinData <- cbind(subjectjoinData[, ], features)
 
 
 ## Uses descriptive activity names to name the activities in the data set
@@ -48,6 +50,12 @@ yjoinData[, 1] <- activityData[yjoinData[, 1], 2]
 
 
 ##Appropriately labels the data set with descriptive variable names. 
-finalData<- cbind(subjectjoinData, yjoinData, xjoinData)
+othrNames <- features2[,2]
+othrNames = gsub('-mean', 'Mean', othrNames)
+othrNames = gsub('-std', 'Std', othrNames)
+othrNames = gsub('[-()]', '', othrNames)
+xjoinData<- xjoinData[, meanstdData]
+finalData<- cbind( yjoinData, subjectjoinData, xjoinData)
+colnames(finalData) <- othrNames
 
 ## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
